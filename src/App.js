c7d382126d46b9b72e9637e371338e9b07bc5f04
ashikraj78 <ashikraj.78@gmail.com>
2020-10-08 19:10:02 +0530

    Add Home page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,82 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+
+function NotFound() {
+  return <h1>Page not Found !</h1>;
+}
+
+function Private(props) {
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function Public(props) {
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false,
+      userDetails: "",
+    };
+  }
+  componentDidMount() {
+    if (localStorage.authTokenConduit) {
+      this.setState({ isLoggedIn: true });
+    }
+  }
+
+  updateLoggedInUser = (user) => {
+    this.setState({
+      isLoggedIn: true,
+      userDetails: user,
+    });
+  };
+
+  handleLogout() {
+    this.setState({ isLoggedIn: false });
+    localStorage.removeItem("authTokenConduit");
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        {this.state.isLoggedIn ? (
+          <Private
+            isLoggedIn={this.state.isLoggedIn}
+            userDetails={this.state.userDetails}
+            handleLogout={this.handleLogout}
+          />
+        ) : (
+          <Public updateLoggedInUser={this.updateLoggedInUser} />
+        )}
+      </BrowserRouter>
     );
   }
 }
