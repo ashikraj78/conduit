01915c18904a108e2521274c137f72f75f46aee9
ashikraj78 <ashikraj.78@gmail.com>
2020-10-08 19:13:06 +0530

    Add component

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from "react";
+import TempelateCard from "./TempelateCard";
+
+class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+    };
+  }
+  componentDidMount() {
+    fetch(
+      "https://mighty-oasis-08080.herokuapp.com/api/articles?limit=10&offset=0"
+    )
+      .then((res) => res.json())
+      .then((articles) => {
+        console.log(articles);
+        this.setState({ articles: articles.articles });
+      });
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="flex">
+          <TempelateCard articles={this.state.articles} />
+        </div>
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ src/components/TempelateCard.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "bulma/css/bulma.css";
+
+function TempelateCard(props) {
+  console.log(props.articles);
+  return (
+    <div>
+      {props.articles.map((article) => (
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-4by3">
+              <img
+                src="https://bulma.io/images/placeholders/1280x960.png"
+                alt="Placeholder"
+              />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img src={article.author.image} alt="athorimage" />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">{article.title}</p>
+                <p className="subtitle is-6">@{article.author.username}</p>
+              </div>
+            </div>
+
+            <div className="content">
+              {article.description}
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              nec iaculis mauris.
+              {article.tagList.map((list) => (
+                <a href="#">#{list}</a>
+              ))}
+              <br />
+              <time datetime="2016-1-1">{article.updatedAt}</time>
+            </div>
+          </div>
+        </div>
+      ))}
+      ;
+    </div>
+  );
+}
+export default TempelateCard;
